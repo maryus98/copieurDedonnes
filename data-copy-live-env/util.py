8e7050bad2de508ff4ea8ac143483e1797028ed4
ayoubrs <ayoubbouras3@gmail.com>
2022-10-04 13:43:14 +0100

    third commit

--- data-copy-live-env/util.py
+++ data-copy-live-env/util.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 def get_tables(path):
     tables_to_be_loaded = pd.read_csv(path, sep=':')
-    return tables_to_be_loaded.query("to_be_loaded='yes'")
+    return tables_to_be_loaded.query("to_be_loaded=='yes'")
