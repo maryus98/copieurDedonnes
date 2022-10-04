9265c45a9e9dd7c1c34156072a6157b0d4b65ef7
ayoubrs <ayoubbouras3@gmail.com>
2022-10-05 00:27:36 +0100

    ip addrees changed

--- data-copy-live-env/config.py
+++ data-copy-live-env/config.py
@@ -4,14 +4,14 @@ DB_DETAILS = {
     'dev':{
         'SOURCE_DB':{
             'DB_TYPE':'mysql',
-            'DB_HOST':'139.99.209.131',
+            'DB_HOST':'192.168.1.119',
             'DB_NAME':'retail_db',
             'DB_USER':os.environ.get('SOURCE_DB_USER'),
-            'DB_PASS':os.environ.get('SOURCE_DB_PASS')
+            'DB_PASS':os.environ.get('SOURCE_sourceDB_PASS')
         },
         'TRAGET_DB':{
             'DB_TYPE':'postgres',
-            'DB_HOST':'139.99.209.131',
+            'DB_HOST':'192.168.1.119',
             'DB_NAME':'retail_db',
             'DB_USER':os.environ.get('TARGET_DB_USER'),
             'DB_PASS':os.environ.get('TARGET_DB_PASS')
