02e2636e179d9e3353831689a3578c8aaea5c406
ayoubrs <ayoubbouras3@gmail.com>
2022-10-06 18:35:13 +0100

    source user name changed

--- data-copy-live-env/config.py
+++ data-copy-live-env/config.py
@@ -7,7 +7,7 @@ DB_DETAILS = {
             'DB_HOST':'192.168.1.119',
             'DB_NAME':'retail_db',
             'DB_USER':os.environ.get('SOURCE_DB_USER'),
-            'DB_PASS':os.environ.get('SOURCE_sourceDB_PASS')
+            'DB_PASS':os.environ.get('SOURCE_DB_PASS')
         },
         'TRAGET_DB':{
             'DB_TYPE':'postgres',
